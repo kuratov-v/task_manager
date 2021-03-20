9d16be41e79c2c33f17e0b4ffdf40aee85f828f4
kuratov-v <kuratovv29@gmail.com>
2021-03-20 19:41:01 +0300

    add filters

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
     "task_manager",
 ]
 
@@ -95,7 +96,8 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
 
 
--- task_manager/views.py
+++ task_manager/views.py
@@ -4,6 +4,8 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class TaskView(
     mixins.ListModelMixin,
@@ -14,6 +16,8 @@ class TaskView(
 ):
     permission_classes = [IsAuthenticated]
     serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status', 'date_end']
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
